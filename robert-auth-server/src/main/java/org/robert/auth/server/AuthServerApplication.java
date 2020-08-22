ed2c0279f0d2da1d7ed09abbedcac39b883a98b1
lwx20200323 <632395785@qq.com>
2020-08-22 17:06:16 +0800

    11

--- robert-auth-server/src/main/java/org/robert/auth/server/AuthServerApplication.java
+++ robert-auth-server/src/main/java/org/robert/auth/server/AuthServerApplication.java
@@ -3,10 +3,13 @@ package org.robert.auth.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import tk.mybatis.spring.annotation.MapperScan;
 
-
-@SpringBootApplication
+@ComponentScan(basePackages = {"org.robert.*"})
 @EnableDiscoveryClient
+@MapperScan(basePackages = {"org.robert.auth.server.mapper"})
+@SpringBootApplication
 public class AuthServerApplication {
 
     public static void main(String[] args) {
