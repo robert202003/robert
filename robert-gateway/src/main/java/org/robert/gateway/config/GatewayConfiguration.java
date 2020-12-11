b5944e7b8b475a7008d74ef51ea34409659bfa27
lwx20200323 <632395785@qq.com>
2020-12-11 19:09:25 +0800

    1

--- robert-gateway/src/main/java/org/robert/gateway/config/GatewayConfiguration.java
+++ robert-gateway/src/main/java/org/robert/gateway/config/GatewayConfiguration.java
@@ -1,24 +1,14 @@
 package org.robert.gateway.config;
 
-import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayFlowRule;
-import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayRuleManager;
-import com.alibaba.csp.sentinel.adapter.gateway.sc.SentinelGatewayFilter;
-import org.robert.gateway.exception.JsonSentinelGatewayBlockExceptionHandler;
 import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.web.reactive.result.view.ViewResolver;
 
-import javax.annotation.PostConstruct;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Configuration
 public class GatewayConfiguration {
@@ -32,39 +22,6 @@ public class GatewayConfiguration {
         this.serverCodecConfigurer = serverCodecConfigurer;
     }
 
-    /**
-     * 配置SentinelGatewayBlockExceptionHandler，限流后异常处理
-     *
-     * @return
-     */
-    @Bean
-    @Order(Ordered.HIGHEST_PRECEDENCE)
-    public JsonSentinelGatewayBlockExceptionHandler sentinelGatewayBlockExceptionHandler() {
-        return new JsonSentinelGatewayBlockExceptionHandler(viewResolvers, serverCodecConfigurer);
-    }
-
-    @Bean
-    @Order(-1)
-    public GlobalFilter sentinelGatewayFilter() {
-        return new SentinelGatewayFilter();
-    }
-
-    @PostConstruct
-    public void doInit() {
-        initGatewayRules();
-    }
-
-    /**
-     * 配置限流规则
-     */
-    private void initGatewayRules() {
-        Set<GatewayFlowRule> rules = new HashSet<>();
-        rules.add(new GatewayFlowRule("/default")
-                .setCount(10) // 限流阈值
-                .setIntervalSec(1) // 统计时间窗口，单位是秒，默认是 1 秒
-        );
-        GatewayRuleManager.loadRules(rules);
-    }
 
     @Bean(name = "messageSource")
     public ResourceBundleMessageSource resourceBundleMessageSource() {
--- robert-gateway/src/main/java/org/robert/gateway/exception/GlobalExceptionConfig.java
+++ robert-gateway/src/main/java/org/robert/gateway/exception/GlobalExceptionConfig.java
@@ -17,6 +17,7 @@ import java.util.List;
 
 @Configuration
 public class GlobalExceptionConfig {
+
     @Autowired
     private AccessLogService accessLogService;
 
