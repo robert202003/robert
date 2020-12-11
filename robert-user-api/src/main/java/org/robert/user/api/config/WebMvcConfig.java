f26565304132f7c3bd522c8cee2c687e4f39281f
lwx20200323 <632395785@qq.com>
2020-12-11 19:04:55 +0800

    1

--- robert-user-api/src/main/java/org/robert/user/api/config/WebMvcConfig.java
+++ robert-user-api/src/main/java/org/robert/user/api/config/WebMvcConfig.java
@@ -5,6 +5,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-public class WebMvcConfig extends DefaultWebMvcConfig {
+public class WebMvcConfig {
 
 }
