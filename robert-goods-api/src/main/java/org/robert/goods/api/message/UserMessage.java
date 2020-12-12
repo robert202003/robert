2ade73eddfde8d6ca63e2c44a4ce4746858fe336
lwx20200323 <632395785@qq.com>
2020-12-12 16:13:03 +0800

    1

--- robert-goods-api/src/main/java/org/robert/goods/api/message/UserMessage.java
+++ robert-goods-api/src/main/java/org/robert/goods/api/message/UserMessage.java
@@ -7,6 +7,8 @@ import java.io.Serializable;
 @Data
 public class UserMessage implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
     private Integer userId;
 
     private String userName;
