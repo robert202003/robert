462a892c7340c0e986077b5259fbd8d6f0e9883f
lwx20200323 <632395785@qq.com>
2020-12-14 09:15:37 +0800

    1

--- robert-file-api/src/main/java/org/robert/file/FileApiApplication.java
+++ robert-file-api/src/main/java/org/robert/file/FileApiApplication.java
@@ -3,11 +3,9 @@ package org.robert.file;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
 
 @EnableDiscoveryClient
-@ComponentScan(basePackages = {"org.robert.*"})
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"org.robert.*"})
 public class FileApiApplication {
     public static void main(String[] args) {
 
